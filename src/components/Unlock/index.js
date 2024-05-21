5f18693badbd497aafd98435f0830ce4e39ca6da
vikasgaddam9010 <nxtwave@ip-10-2-76-201.ap-south-1.compute.internal>
2024-05-21 12:33:32 +0000

    Lock Unlock Hooks

---
+++ src/components/Unlock/index.js
@@ -0,0 +1,32 @@
+import {MainContainer, Img, Heading, Button} from './styledComponents'
+import {useState} from 'react'
+
+const Unlock = () => {
+  const [lockState, setLockState] = useState(false)
+
+  const btnClicked = () => {
+    setLockState(prevState => !prevState)
+  }
+
+  console.log(lockState)
+
+  const lockImg = lockState
+    ? 'https://assets.ccbp.in/frontend/hooks/lock-img.png'
+    : 'https://assets.ccbp.in/frontend/hooks/unlock-img.png'
+  const imgAlt = lockState ? 'lock' : 'unlock'
+  const paraElement = lockState
+    ? 'Your Device is Locked'
+    : 'Your Device is Unlocked'
+  const btnText = lockState ? 'Unlock' : 'Lock'
+  return (
+    <MainContainer>
+      <Img alt={imgAlt} src={lockImg} />
+      <Heading>{paraElement}</Heading>
+      <Button type="bttuon" onClick={btnClicked}>
+        {btnText}
+      </Button>
+    </MainContainer>
+  )
+}
+
+export default Unlock
---
+++ src/components/Unlock/styledComponents.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-image: linear-gradient(#161617, #3c2940);
+color: #e2e8f0;
+@media (max-width: 576px){
+    min-width: 450px;
+}
+`
+export const Img = styled.img`
+width: 150px;
+`
+
+export const Heading = styled.h1`
+margin-bottom: 150px;
+@media(max-width: 576px){
+    margin-bottom: 50px;
+    font-size: 25px;
+}
+`
+
+export const Button = styled.button`
+background-color:#06b6d4;
+border: none;
+border-radius: 10px;
+color: #e2e8f0;
+font-weight: bold;
+padding: 15px;
+padding-left: 29px;
+padding-right: 29px;
+`
